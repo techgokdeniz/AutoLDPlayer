3e883938f81ac407e9d7384b673507aa33c311c2
Gökdeniz Çetin <201913709072@ogr.balikesir.edu.tr>
2023-05-31 21:09:06 +0300

    Revert "add logger and ldplayer class"
    This reverts commit 28b756cbb63d532045d061155461d0ab8b8352d5.

--- index.js
+++
@@ -1,9 +0,0 @@
-import LDPlayer from "./LDPlayer.js";
-
-const LDPlayerPath = "C:\\LDPlayer\\LDPlayer64\\ldconsole.exe";
-
-(function () {
-  const player = new LDPlayer(LDPlayerPath);
-  player.start();
-  player.quaitAll();
-})();
